a009bfc37dcb290ed623ce3eee6b9eb508dd4d2a
Anjali singh <125783824+anjali-1s@users.noreply.github.com>
2023-02-22 01:23:18 +0530

    Add files via upload

---
+++ pattern01.cpp
@@ -0,0 +1,26 @@
+// write a program to print the following pattern ( for n = 4 ) :
+/*
+    1 2 3 4 
+    1 2 3 4 
+    1 2 3 4
+    1 2 3 4
+*/
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cout<<" Enter the value of n :"<<endl;
+    cin>>n;
+    int i = 1;
+    while ( i <= n){
+        int j = 1;
+        while ( j <= n){
+            cout<<j<<" ";
+             j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
